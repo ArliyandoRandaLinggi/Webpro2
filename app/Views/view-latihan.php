f92182f0f8da494d486845488f4d878825072733
ArliyandoRandaLinggi <arliyando2804@gmail.com>
2023-04-10 14:24:56 +0700

    contoh 3

--- app/Views/view-latihan.php
+++ app/Views/view-latihan.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-    <h1>Halo Kawan Semua Yuk Kita Belajar Web Programming...!!!!</h1>
+    <h1>Halo Kawan Semua Yuk Kita Belajar Web Programming...!!!</h1>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
     ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
