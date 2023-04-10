c69b81f783978cbca21deccbd7ac0c3d990f89b8
ArliyandoRandaLinggi <arliyando2804@gmail.com>
2023-04-10 14:10:54 +0700

    menambahkan Tugas Webpro2

---
+++ app/Views/contoh1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh 1</title>
+</head>
+
+<body>
+    <h1>Perkenalkan<p>Nama Saya Imam Nawawi
+            Saya tinggal di daerah Ciputat
+            olahraga yang saya sukai adalah bulutangkis</p>
+
+
+    </h1>
+</body>
+
+</html>
---
+++ app/Views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h1>Halo Kawan Semua Yuk Kita Belajar Web Programming...!!!!</h1>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+
+</html>
